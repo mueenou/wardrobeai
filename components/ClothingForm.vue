a64e4aa6cae2dc1edcefc0f6ec86c2bac4669a81
mueenou <mueen.hossain@gmail.com>
2025-05-18 18:50:59 +0200

    WIP: modal installation

--- components/ClothingForm.vue
+++ components/ClothingForm.vue
@@ -13,7 +13,8 @@
         :options="sexeOptions"
         :model-value="sexe"
       /> -->
-      <div class="flex gap-4 w-fit mx-auto items-center">
+      <h2 class="font-bold uppercase text-lg">Main Informations</h2>
+      <div class="flex gap-4 w-fit items-center">
         <UFormGroup label="What is your gender?" size="sm" class="my-2">
           <GenderSelector v-model="sexe" />
         </UFormGroup>
@@ -26,57 +27,7 @@
           />
         </UFormGroup>
       </div>
-      <UFormGroup label="Clothing type" size="sm" class="my-2">
-        <!-- <UInputMenu
-          color="primary"
-          v-model="selectedClothingType"
-          :options="clothingTypes"
-          icon="mingcute:t-shirt-fill"
-        /> -->
-        <ClothImageInput v-model="selectedClothingType" />
-      </UFormGroup>
-      <UFormGroup label="Cloth color" size="sm" class="my-2">
-        <!-- <UInputMenu
-          color="primary"
-          v-model="selectedColor"
-          :options="clothingColors"
-          placeholder="Select a person"
-          by="id"
-          option-attribute="name"
-          :search-attributes="['name', 'color']"
-        >
-          <template #option="{ option: set }">
-            <span :class="`h-3 w-8 rounded`" :style="`background-color: ${set.hex}`" />
-            <span class="truncate">{{ set.name }}</span>
-          </template>
-        </UInputMenu> -->
-        <ColorSelector v-model="selectedColor" />
-      </UFormGroup>
-      <div class="flex flex-wrap gap-x-4 items-center">
-        <UFormGroup label="Clothing fabric" size="sm" class="my-2">
-          <UInputMenu
-            color="primary"
-            v-model="selectedFabric"
-            :options="clothingFabrics"
-            icon="mdi:fabric"
-          />
-        </UFormGroup>
-        <!-- <UFormGroup label="Weather suitability" size="sm" class="my-2">
-        <UInputMenu
-          color="primary"
-          v-model="selectedWeatherSuitability"
-          :options="weatherSuitability"
-          icon="material-symbols:matter"
-        />
-      </UFormGroup> -->
-        <UFormGroup label="Style" size="sm" class="my-2">
-          <UInputMenu
-            color="primary"
-            v-model="selectedStyleTag"
-            :options="styleTags"
-            icon="material-symbols:style"
-          />
-        </UFormGroup>
+      <div class="flex gap-4 w-fit items-center">
         <UFormGroup label="Destination (optional)" size="sm" class="my-2">
           <UInput
             @input="outfitStore.updateDestination($event.target.value)"
@@ -107,17 +58,13 @@
           </UPopover>
         </UFormGroup>
       </div>
-      <div class="flex gap-x-4"></div>
-      <div class="w-full justify-end text-right">
-        <UButton
-          @click="addClothToList"
-          label="Add cloth"
-          color="indigo"
-          variant="outline"
-          size="md"
-          class="mt-2"
-        />
-      </div>
+      <template>
+        <UModal title="Modal with title">
+          <UButton label="Open" color="neutral" variant="subtle" />
+
+          <template #body> </template>
+        </UModal>
+      </template>
     </form>
     <ClothTable v-model="clothesList" />
     <div class="text-right" v-if="clothesList.length > 0">
@@ -136,11 +83,6 @@
 <script setup>
 import { storeToRefs } from "pinia";
 import { useOutfitStore } from "~/stores/outfit";
-import { CLOTHING_TYPES } from "~/constants/clothing";
-import { CLOTHING_COLORS } from "~/constants/clothing";
-import { FABRICS } from "~/constants/clothing";
-// import { WEATHER_SUITABILITY } from "~/constants/clothing";
-import { STYLE_TAGS } from "~/constants/clothing";
 import { ETHNICITIES } from "~/constants/clothing";
 import { format } from "date-fns";
 
@@ -148,24 +90,13 @@ const outfitStore = useOutfitStore();
 const { clothesList, sexe, ethnicity, tripDates } = storeToRefs(outfitStore);
 const emit = defineEmits(["generate"]);
 
+const ethnicities = ETHNICITIES;
+
 // update the trip dates in the store
 const selected = computed({
   get: () => tripDates.value,
   set: (newDates) => outfitStore.updateTripDates(newDates),
 });
-
-// Constantes pour les options des selects
-const clothingFabrics = FABRICS;
-const ethnicities = ETHNICITIES;
-// const weatherSuitability = WEATHER_SUITABILITY;
-const styleTags = STYLE_TAGS;
-
-// Refs pour le formulaire
-const selectedClothingType = ref(CLOTHING_TYPES[0]);
-const selectedColor = ref(CLOTHING_COLORS[0]);
-const selectedFabric = ref(FABRICS[0]);
-// const selectedWeatherSuitability = ref(WEATHER_SUITABILITY[0]);
-const selectedStyleTag = ref(STYLE_TAGS[0]);
 
 const toast = useToast();
 // Fonction pour ajouter un vêtement
---
+++ components/ClothingModal.vue
@@ -0,0 +1,58 @@
+<template>
+  <form>
+    <UFormGroup label="Clothing type" size="sm" class="my-2">
+      <ClothImageInput v-model="selectedClothingType" />
+    </UFormGroup>
+    <UFormGroup label="Cloth color" size="sm" class="my-2">
+      <ColorSelector v-model="selectedColor" />
+    </UFormGroup>
+    <div class="flex flex-wrap gap-x-4 items-center">
+      <UFormGroup label="Clothing fabric" size="sm" class="my-2">
+        <UInputMenu
+          color="primary"
+          v-model="selectedFabric"
+          :options="clothingFabrics"
+          icon="mdi:fabric"
+        />
+      </UFormGroup>
+      <UFormGroup label="Style" size="sm" class="my-2">
+        <UInputMenu
+          color="primary"
+          v-model="selectedStyleTag"
+          :options="styleTags"
+          icon="material-symbols:style"
+        />
+      </UFormGroup>
+    </div>
+    <div class="w-full justify-end text-right">
+      <UButton
+        @click="addClothToList"
+        label="Add cloth"
+        color="indigo"
+        variant="outline"
+        size="md"
+        class="mt-2"
+      />
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { CLOTHING_TYPES } from "~/constants/clothing";
+import { CLOTHING_COLORS } from "~/constants/clothing";
+import { FABRICS } from "~/constants/clothing";
+import { STYLE_TAGS } from "~/constants/clothing";
+
+// Constantes pour les options des selects
+const clothingFabrics = FABRICS;
+
+const styleTags = STYLE_TAGS;
+
+// Refs pour le formulaire
+const selectedClothingType = ref(CLOTHING_TYPES[0]);
+const selectedColor = ref(CLOTHING_COLORS[0]);
+const selectedFabric = ref(FABRICS[0]);
+const selectedStyleTag = ref(STYLE_TAGS[0]);
+</script>
+
+<style scoped></style>
